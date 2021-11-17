e10295b8e8f06f52c6b7fe716f18d6bf7e2f40cc
xie <2761817351@qq.com>
2021-11-17 22:40:32 +0800

    myComponents

---
+++ React_Thumbnail/index.js
@@ -0,0 +1,28 @@
+import React, {useEffect, useState} from 'react';
+
+import './index.css';
+
+export default function Menu(props){
+    let content = {title:{url:"#",firstTitle:"I am Title1",secondTitle:"I am Title2"}};
+    let img = {toUrl:"#",url:"./img/Thumbnail/first.png",name:"title"};
+    let text = "文本内容文本内容文本内容文本内容文本内容文本内容";
+
+    return(
+        <div className={"TN_container"}>
+            {/*缩略图容器*/}
+            <div><a href={img.toUrl}>
+                {/*缩略图*/}
+                <img src={img.url} alt={img.name}/>
+            </a></div>
+
+            <div className={"TN_content"}>
+                {/*缩略图问题内容*/}
+                <h2><a href={content.title.url}>
+                    {content.title.firstTitle} <br/>
+                    <small>{content.title.secondTitle}</small>
+                </a></h2>
+                <p>{text}</p>
+            </div>
+        </div>
+    )
+}

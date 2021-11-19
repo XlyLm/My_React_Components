99afc908da70f0e9c5dd947a7fb0dddcf67f213c
xie <2761817351@qq.com>
2021-11-19 15:45:09 +0800

    myComponents

---
+++ React_Aside/index.js
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from 'react';
+
+import './index.css';
+import './iconfont/iconfont.css';
+
+export default function Menu(props){
+    // 此处用于初始化数据
+    useEffect(()=>{
+        return null;
+    });
+
+    return(
+        // 菜单容器
+        <div className={"aside_container"}>
+            <a href="#/" className="block">
+                <span className="iconfont icon-shouji"></span><br/>
+                <p>手机APP</p>
+            </a>
+            <a href="#/" className="block">
+                <span className="iconfont icon-ren"></span><br/>
+                <p>个人中心</p>
+            </a>
+            <a href="#/" className="block">
+                <span className="iconfont icon-spanner-fill"></span><br/>
+                <p>售后服务</p></a>
+            <a href="#/" className="block">
+                <span className="iconfont icon-24gf-phoneLoudspeaker"></span><br/>
+                <p>人工客服</p></a>
+            <a href="#/" className="block">
+                <span className="iconfont icon-gouwuche"></span><br/>
+                <p>购物车</p></a>
+            <a href="#top" className="hide-last">
+                <span className="iconfont icon-xiangshangjiantou"></span><br/>
+                <p>回顶部</p>
+            </a>
+        </div>
+    )
+}
